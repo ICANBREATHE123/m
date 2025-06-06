0e05199e25ab7c980069305b19a0d94f59ff9237
ICANBREATHE123 <Lucas.leclerq@gmail.com>
2025-06-06 10:46:53 +0200

    versie 3

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a78cb5b81f3b37caff9ac51e057fc3a78bcf7c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a40f2f8f704d298d6a2ff2447fccc187e7aaa44d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
