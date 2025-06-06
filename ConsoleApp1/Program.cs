2a78cb5b81f3b37caff9ac51e057fc3a78bcf7c3
ICANBREATHE123 <Lucas.leclerq@gmail.com>
2025-06-06 03:02:12 +0200

    versie 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Boek boek = new Boek("De Avonturen van Tom Sawyer", "Mark Twain", 300);
+class Boek
+{
+    public string Titel;
+    public string Auteur;
+    public int PaginaAantal;
+
+    public Boek(string titel, string auteur, int paginaAantal)
+    {
+        Titel = titel;
+        Auteur = auteur;
+        PaginaAantal = paginaAantal;
+    }
+
+    public override string ToString()
+    {
+        return $"{Titel} - {Auteur} ({PaginaAantal} pagina's)";
+    }
+}
